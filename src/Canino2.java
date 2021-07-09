47cebaaf1c4a5b3c2ed938990288bd5fb696e6bd
Giotek27 <serleonard123@gmail.com>
2021-07-08 21:10:01 -0500

    app_caninos

---
+++ src/Canino2.java
@@ -0,0 +1,12 @@
+public abstract class Canino2 {
+    private String aullan;
+
+    public String getAullan() {
+        return aullan;
+    }
+    public void setAullan(String aullan) {
+        this.aullan = aullan;
+    }
+
+    public abstract void cazar();
+}
